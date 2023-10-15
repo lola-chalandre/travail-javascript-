53595425239ecbc05718e9afeb461db90b181287
lola-chalandre <147378400+lola-chalandre@users.noreply.github.com>
2023-10-15 23:35:07 +0200

    Add files via upload

---
+++ ex pokemon.js
@@ -0,0 +1,58 @@
+let 'pokemon1' = 'Mistigrix'
+let 'pokemon2' = 'Ceribou'
+
+class Pokemon1 {
+    constructor(name, attack, defense, hp, luck) {
+     this.name = Mistigrix 
+     this.attack = 48
+     this.defense = 76
+     this.hp = 74
+     this.luck = 86
+    }
+    
+class Pokemon2 {
+    constructor(name, attack, defense, hp, luck) {
+    this.name = Ceribou
+    this.attack = 35
+    this.defense = 45
+    this.hp = 45
+    this.luck = 30
+   }
+
+    isLuck() {
+        luckpoke1 = Math.floor(Math.random());
+        luckpoke1 = Math.floor(Math.random())
+        if (luckpoke1 < luckpoke2){
+            pokeattack = pokeattack 2();
+            pokeattack = pokeattack 1();
+        } else {
+            pokeattack = pokemon1();
+            pokeattack = pokemon2();
+        }
+    }
+    attackpokemon() {
+        this.isluck(),
+        degatinflige = pokeattack.attack() - pokeattack.defense();
+        pokeattack.hp() -= degatinflige;
+        console.log(pokeattack +' a subi '+ degatinflige +' de degat, il lui reste alors' + pokeattack.hp() + 'point de vie');
+    }
+}
+ let pokemon1 = new pokemon('mistigrix', 48, 76, 74, luckpoke1) 
+ let pokemon2 = new pokemon('ceribou', 35, 45, 45 , luckpoke2) 
+
+ while ( pokemon1.hp > 0 && pokemon2 > 0) {
+    isluck(),
+    attack();
+ }
+ if (pokemon1.hp <= 0){
+    console.log(pokemon1.name +' est mort ');
+    console.log(pokemon2.name +' a gagne ');
+ } else if (pokemon2.hp < 0) {
+    console.log(pokemon2.name +' est mort ');
+    console.log( pokemon1.name +' a gagne ');
+ }
+    
+
+
+    
+
